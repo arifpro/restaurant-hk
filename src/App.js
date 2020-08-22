25b17fb462409f699ae8fb048c75beeb2b90b7d2
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-08-22 14:50:21 +0800

    click card to show details

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import './App.css';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './components/Menu/Menu';
+import { DISHES } from './shared/dishes'
 
 function App() {
   return (
     <div>
       <Navbar dark color="primary">
         <div className="container">
-          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          <NavbarBrand href="/">Restaurant Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu />
+      <Menu dishes={DISHES} />
     </div>
   );
 }
--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,59 +1,40 @@
 import React, { useState } from 'react';
-import { Media } from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
-const Menu = (props) => {
-    const data = [
-        {
-            id: 0,
-            name:'Uthappizza',
-            image: 'assets/images/uthappizza.png',
-            category: 'mains',
-            label:'Hot',
-            price:'4.99',
-            description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'
-        },
-        {
-            id: 1,
-            name:'Zucchipakoda',
-            image: 'assets/images/zucchipakoda.png',
-            category: 'appetizer',
-            label:'',
-            price:'1.99',
-            description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'
-        },
-        {
-            id: 2,
-            name:'Vadonut',
-            image: 'assets/images/vadonut.png',
-            category: 'appetizer',
-            label:'New',
-            price:'1.99',
-            description:'A quintessential ConFusion experience, is it a vada or is it a donut?'
-        },
-        {
-            id: 3,
-            name:'ElaiCheese Cake',
-            image: 'assets/images/elaicheesecake.png',
-            category: 'dessert',
-            label:'',
-            price:'2.99',
-            description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'
+const Menu = ({ dishes }) => {
+    const [selectedDish, setSelectedDish] = useState(null);
+
+    const onDishSelect = (dish) => {
+        setSelectedDish(dish);
+        console.log(dish);
+    };
+
+    const renderDish = (dish) => {
+        if (dish != null) {
+            return (
+                <Card>
+                    <CardImg width="50%" src={dish.image} alt={dish.name} />
+                    <CardBody>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
+                    </CardBody>
+                </Card>
+            ) 
+        } else {
+            return (
+                <div></div>
+            )
         }
-    ];
-    const [dishes, setDishes] = useState(data);
-
+    };
 
     const menu = dishes.map(dish => (
-            <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                    <Media left middle>
-                        <Media object src={dish.image} alt={dish.name} />
-                    </Media>
-                    <Media body className="ml-5">
-                        <Media heading>{dish.name}</Media>
-                        <p>{dish.description}</p>
-                    </Media>
-                </Media>
+            <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card onClick={() => onDishSelect(dish)}>
+                    <CardImg width="50%" src={dish.image} alt={dish.name} />
+                    <CardImgOverlay>
+                        <CardTitle>{dish.name}</CardTitle>
+                    </CardImgOverlay>
+                </Card>
             </div>
         )
     );
@@ -61,9 +42,12 @@ const Menu = (props) => {
     return (
         <div className="container">
             <div className="row">
-                <Media list>
-                    {menu}
-                </Media>
+                {menu}
+            </div>
+            <div className="row">
+                <div className="col-12 col-md-5 m-1">
+                    {renderDish(selectedDish)}
+                </div>
             </div>
         </div>
     );
