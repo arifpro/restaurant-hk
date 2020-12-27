9f7691e3f1470f7683bed3a1197bcaa575441767
MD ARIF HOSSAIN <arif.swfu@outlook.com>
2020-12-27 14:34:02 +0800

    dish actions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,8 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+
+// reducers
 import { Dishes } from './reducers/dishes';
 import { Comments } from './reducers/comments';
 import { Promotions } from './reducers/promotions';
@@ -11,7 +15,8 @@ export const ConfigureStore = () => {
             comments: Comments,
             promotions: Promotions,
             leaders: Leaders
-        })
+        }),
+        applyMiddleware(thunk, logger)
     );
 
     return store;
--- src/redux/reducers/dishes.js
+++ src/redux/reducers/dishes.js
@@ -1,8 +1,22 @@
+import * as ActionTypes from './ActionTypes';
 import { DISHES } from '../../shared/dishes';
 
-export const Dishes = (state = DISHES, action) => {
+export const Dishes = (state = {
+    isLoading: true,
+    errMess: null,
+    dishes:[]
+}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_DISHES:
+            return {...state, isLoading: false, errMess: null, dishes: action.payload};
+
+        case ActionTypes.DISHES_LOADING:
+            return {...state, isLoading: true, errMess: null, dishes: []}
+
+        case ActionTypes.DISHES_FAILED:
+            return {...state, isLoading: false, errMess: action.payload};
+
         default:
             return state;
-    };
+    }
 };
